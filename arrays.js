7193bee43a07e44981a6a939d33d8f59514c5dc6
Daniel Gould <dmgould310@gmail.com>
2020-06-09 19:40:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, name) {
+function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningofArray(array, name){
-  array.unshift(name);
+function destructivelyAddElementToBeginningofArray(array, element){
+  array.unshift(element);
   return array
 }
 

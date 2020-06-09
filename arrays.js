cd587ab6b45f31eb52f832be5d6ff1a515ea9738
Daniel Gould <dmgould310@gmail.com>
2020-06-09 19:29:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 //var candyString = "foo"
-function addElementToBeginningofArray(name){
-  return (name,...chocolateBars)
-}
+
 
 
 

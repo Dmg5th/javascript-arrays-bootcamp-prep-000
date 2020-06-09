5794dd1974106121e15b6b627071e01e95ee577b
Daniel Gould <dmgould310@gmail.com>
2020-06-09 19:40:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningofArray(array, element){
   array.unshift(element);
-  return array
+  returnl array
 }
 
 
